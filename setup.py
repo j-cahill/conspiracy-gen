eef0936188156525be5313320814e19a813b96f4
Jesse <jcahill225@gmail.com>
2020-10-12 14:45:01 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An NLP tool to generate conspiracy theories based on the content of Reddit's r/conspiracy',
+    author='Jesse Cahill',
+    license='MIT',
+)
